ba28a61daf9fc5698cc58d21f74308e39439ceb5
millena ferreira <millenamills44@gmail.com>
2023-02-12 19:24:05 -0300

    Atividade 1 finalizada

--- Lista de exercicio/Exercicio 1/app.js
+++ Lista de exercicio/Exercicio 1/app.js
@@ -4,3 +4,120 @@
  * Data: 10/02/2023
  * Versão: 1.0
  **************************************************************/
+
+
+//Importe da biblioteca media
+var calcularMedia = require('./modulo/media.js');
+ 
+var readline = require('readline');
+
+var entradaDados = readline.createInterface({
+    input : process.stdin,
+    output: process.stdout
+});
+
+//nome do aluno
+entradaDados.question('Nome do Aluno: \n', function(nomeDoAluno){
+    let nomeAluno = nomeDoAluno;
+
+    //nome do professor
+    entradaDados.question('Nome do Professor: \n', function(nomeDoProfessor){
+        let nomeProfessor = nomeDoProfessor;
+
+        //sexo do aluno
+        entradaDados.question('Sexo do Aluno: F/M \n', function(sexoDoAluno){
+            let sexoAluno = sexoDoAluno;
+
+            //sexo do professor
+            entradaDados.question('Sexo do Professor: F/M \n', function(sexoDoProfessor){
+                let sexoProfessor = sexoDoProfessor;
+
+                //nome do curso
+                entradaDados.question('Nome do Curso: \n', function(nomeDoCurso){
+                    let nomeCurso = nomeDoCurso;
+
+                    //nome da disciplina
+                    entradaDados.question('Nome da disciplina: \n', function(nomeDaDisciplina){
+                        let nomeDisciplina = nomeDaDisciplina;
+
+                        //nota 1
+                        entradaDados.question('Digite nota 1: \n', function(nota1){
+                            let primeiraNota = nota1.replace(',','.');
+
+                            //nota 2
+                            entradaDados.question('Digite nota 2: \n', function(nota2){
+                                let segundaNota = nota2.replace(',','.');
+
+                                //nota 3
+                                entradaDados.question('Digite nota 3: \n', function(nota3){
+                                    let terceiraNota = nota3.replace(',','.');
+
+                                    //nota 4
+                                    entradaDados.question('Digite nota 4: \n', function(nota4){
+                                        let quartaNota = nota4.replace(',','.');
+
+                                        let sexAluno = calcularMedia.sexAl(sexoAluno);
+                                        let sexProfessor = calcularMedia.sexProf(sexoProfessor);
+
+                                        let mediaAluno = calcularMedia.getCalcularMedia(nota1,nota2,nota3,nota4);
+                                        console.log(mediaAluno);
+
+                                        if(mediaAluno >= 50 && mediaAluno <= 69){
+                                            console.log('O aluno precisa fazer o exame');
+                                            entradaDados.question('Digite a nota do exame: \n', function(notaExame){
+
+                                                let resultado = calcularMedia.getExame(mediaAluno, notaExame.replace(',','.'));
+
+                                                if(resultado <= 59){
+                                                    
+                                                    console.log(`${sexAluno} ${nomeAluno} foi REPROVADO na disciplina ${nomeDisciplina}`);
+                                                    console.log(`Curso: ${nomeCurso}`);
+                                                    console.log(`${sexProfessor} : ${nomeProfessor}`);
+                                                    console.log(`Notas do Aluno: ${primeiraNota}, ${segundaNota}, ${terceiraNota}, ${quartaNota}, ${notaExame}`);
+                                                    console.log(`Média Final: ${mediaAluno}`);
+                                                    console.log(`Média do Exame: ${resultado}`);
+
+
+                                                }else if(resultado >= 60){
+
+                                                    console.log(`${sexAluno} ${nomeAluno} foi APROVADO na disciplina ${nomeDisciplina}`);
+                                                    console.log(`Curso: ${nomeCurso}`);
+                                                    console.log(`${sexProfessor} : ${nomeProfessor}`);
+                                                    console.log(`Notas do Aluno: ${primeiraNota}, ${segundaNota}, ${terceiraNota}, ${quartaNota}, ${notaExame}`);
+                                                    console.log(`Média Final: ${mediaAluno}`);
+                                                    console.log(`Média do Exame: ${resultado}`);
+
+                                                }
+                                            });
+
+                                        }
+
+                                        else if(mediaAluno < 50){
+    
+                                            console.log(`${sexAluno} ${nomeAluno} foi REPROVADO na disciplina ${nomeDisciplina}`);
+                                            console.log(`Curso: ${nomeCurso}`);
+                                            console.log(`${sexProfessor} : ${nomeProfessor}`);
+                                            console.log(`Notas do Aluno: ${primeiraNota}, ${segundaNota}, ${terceiraNota}, ${quartaNota}`);
+                                            console.log(`Média Final: ${mediaAluno} `);
+
+                                        }else if(mediaAluno > 70){
+
+                                            console.log(`${sexAluno} ${nomeAluno} foi APROVADO na disciplina ${nomeDisciplina}`);
+                                            console.log(`Curso: ${nomeCurso}`);
+                                            console.log(`${sexProfessor} : ${nomeProfessor}`);
+                                            console.log(`Notas do Aluno: ${primeiraNota}, ${segundaNota}, ${terceiraNota}, ${quartaNota}`);
+                                            console.log(`Média Final: ${mediaAluno} `);
+
+                                        }
+                                        
+
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
--- Lista de exercicio/Exercicio 1/modulo/media.js
+++ Lista de exercicio/Exercicio 1/modulo/media.js
@@ -6,62 +6,93 @@
  **************************************************************/
 
 //função que irar gerar a nota e fazer a validação de si propria
-const getCalcularMedia = function(nota1, nota2, nota3, nota4){
+const getCalcularMedia = function (nota1, nota2, nota3, nota4) {
 
-    let media = (Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4)) /4 ;
-
+    let media = (Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4)) / 4;
 
     //Tratamentos
     //validação de entrada vazia
-    if(nota1 == '' || nota2 == '' || nota3 == '' || nota4 == ''){
+    if (nota1 == '' || nota2 == '' || nota3 == '' || nota4 == '') {
         console.log('ERRO: entrada está vazia');
 
-    //validação de letras
-    }else if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4)){
+        //validação de letras
+    } else if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4)) {
         console.log('ERRO: Digite número válido e não letras');
 
-    //validação se for maior que 100
-    }else if (nota1 > 100 || nota2 > 100 || nota3 > 100 || nota4 > 100){
+        //validação se for maior que 100
+    } else if (nota1 > 100 || nota2 > 100 || nota3 > 100 || nota4 > 100) {
         console.log('ERRO: A nota não pode ser maior que 100');
 
-    //validação se for menor que 0
-    }else if (nota1 < 0 || nota2 < 0 || nota3 < 0 || nota4 < 0){
+        //validação se for menor que 0
+    } else if (nota1 < 0 || nota2 < 0 || nota3 < 0 || nota4 < 0) {
         console.log('ERRO: A nota não pode ser menor que 0');
 
-    //lógica da media
-    }else{
-        if(media <= 50){
-            console.log(media + '\nREPROVADO');
-        }else if(media >= 50 && media <= 69){
-            exame()
-            console.log('exame');
-        }else if(media >= 70){
-            console.log(media + '\nAPROVADO');
+        //lógica da media
+    } else {
+        if (media <= 50) {
+            return media;
+        } else if (media >= 50 && media <= 69) {
+            getExame()
+            return media;
+        } else if (media >= 70) {
+            return media;
         }
     }
 
 }
 
 
+const getExame = function (mediaAluno, notaExame) {
 
-const exame = function(getCalcularMedia,notaExame){
+    let exame = (Number(mediaAluno) + Number(notaExame)) / 2;
 
-    let exame = (getCalcularMedia + notaExame) / 2;
+    if (exame == '') {
+        console.log('ERRO: entrada está vazia');
+    } else if (isNaN(exame)) {
+        console.log('ERRO: Digite número válido e não letras');
+    } else if (exame > 100) {
+        console.log('ERRO: A nota não pode ser maior que 100');
+    } else if (exame < 0) {
+        console.log('ERRO: A nota não pode ser menor que 0');
+    } else {
+        if (exame <= 59) {
+            return exame;
+        } else if (exame >= 60)
+            return exame;
+    }
+}
 
-    if(exame <= 59){
-        console.log(exame + '\nReprovado');
-    }else if(exame >= 60)
-        console.log(exame + '\nAprovado');
-    
+const sexAl = function (sexoAluno) {
+
+    if (sexoAluno == '') {
+
+    } else {
+        if (sexoAluno == 'f' || sexoAluno == 'F' ) {
+            return 'A Aluna'
+        } else {
+            return 'O Aluno'
+        }
+    }
+
 
 }
-getCalcularMedia(40,45,50,60);
-//exame(47, 70);
+
+const sexProf = function (sexoProfessor) {
+
+    if (sexoProfessor == 'f' || sexoProfessor == 'F') {
+        return 'Professora'
+    } else {
+        return 'Professor'
+    }
+
+}
 
 module.exports = {
     getCalcularMedia,
-    exame
+    getExame,
+    sexAl,
+    sexProf
 };
 
-49
+
 
